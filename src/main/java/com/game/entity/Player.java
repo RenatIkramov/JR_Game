7db9a7fffa55c0a07e60a2c06bc0974e0055c685
”Renat <ikramov.renat@gmail.com>
2022-09-08 00:14:59 +0600

    first commit

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -1,7 +1,5 @@
 package com.game.entity;
 
-import org.springframework.data.repository.query.Param;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
--- src/main/java/com/game/repository/PlayerJpaRepository.java
+++ src/main/java/com/game/repository/PlayerJpaRepository.java
@@ -4,8 +4,6 @@ import com.game.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface PlayerJpaRepository extends JpaRepository<Player,Long> {
 
